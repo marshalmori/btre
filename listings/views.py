506f6e384f6fb24857c6d405cc5574f2ffd1b7eb
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2026-04-22 22:34:49 -0300

    Cria a página search.html

--- listings/views.py
+++ listings/views.py
@@ -3,6 +3,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
+from .choices import bedroom_choices, price_choices, state_choices
 from .models import Listing
 
 
@@ -28,4 +29,11 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+    }
+
+    return render(request, 'listings/search.html', context)
